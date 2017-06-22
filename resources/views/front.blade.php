3d629240204121522d5ff933e199b6dc94931b91
Sunil Subedi <sunilsubedi9@gmail.com>
2017-06-22 17:10:25 +0545

    Layout Building with blade

---
+++ resources/views/front.blade.php
@@ -0,0 +1,7 @@
+@extends('frontend.template.main')
+@section('title','| Welcome')
+@section('page-header')
+<div class="container">
+
+   <a href="#" class="btn btn-success btn-large" style="margin:200px 500px 350px">Explore US</a>
+</div>
---
+++ resources/views/frontend/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('frontend/template/main')
+@section('title','| home')
+@section('page-header')
+<div class="container">
+   hello world
+</div>
+
+@endsection
